795dadb73a12d560329cdd835488cfc0440f0982
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-12-26 22:59:43 +0000

    Further work

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,8 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::{gdt, print, println};
+use crate::{exit_qemu, gdt, hlt_loop, print, println, QemuExitCode};
 
 use lazy_static::lazy_static;
+use pc_keyboard::KeyCode;
 use pic8259::ChainedPics;
 
 pub const PIC_1_OFFSET: u8 = 32;
@@ -14,6 +15,7 @@ pub static PICS: spin::Mutex<ChainedPics> =
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard
 }
 
 impl InterruptIndex {
@@ -37,7 +39,9 @@ lazy_static! {
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
         idt[InterruptIndex::Timer.as_u8()]
-            .set_handler_fn(timer_interrupt_handler); // new
+            .set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_u8()]
+            .set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -50,6 +54,47 @@ extern "x86-interrupt" fn timer_interrupt_handler(
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: InterruptStackFrame)
+{
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(ScancodeSet1::new(),
+                layouts::Us104Key, HandleControl::Ignore)
+            );
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => {
+                    if key == KeyCode::F4 && keyboard.get_modifiers().lalt {
+                        println!("Exiting Qemu due to Alt+F4");
+                        exit_qemu(QemuExitCode::Success);
+                        hlt_loop();
+                    }
+
+                    print!("{:?}", key)
+                },
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
 
